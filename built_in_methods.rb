50ed8fd70ad285e720637a5510cf7ee41867df91
Gabriel Torres <thomastgtorres21@gmail.com>
2023-04-02 18:13:08 -0500

    Add built methods

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+#The boolean method is called on the string object "Hello World"
+#The arguement in question is "Hello". The question is asking if the string "Hello World" includes the word hello.
+#The return value is "true"
 "Hello World".include?("Hello")
 
-
+#The boolean method is called on the string object "Hello World"
+#The argument in question is "Hello". The question is asking if the string "Hello World" ends with an "Hello".
+#The return value is false
 "Hello World".end_with?("Hello")
 
-
+#The boolean method is called on the string object "Hello World"
+#The argument in question is "rld". The question is asking if the string "Hello World" ends in "rld"
+#The return value is true
 "Hello World".end_with?("rld")
 
-
+#The boolean method is called on the integer object 12. 
+#There is no argument. The method is deciding if the integer is even or not.
+#The return value is true. 
 12.even?
 
-
+#The boolean method is called on the integer object 18.
+#There is no argument. The method is deciding what number comes after the integer 18
+#The return value is 19
 18.next
 
 
@@ -39,6 +49,18 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+#The strip! method is called on the crystal_types variable, which stores the string object "       lapis_lazuli "
+#The return value is "lapis_lazuli" because "        lapis_lazuli  " has leading and recieving spaces within the string object
+#The puts command prints the result of the strip! method and removes all whitespace within the string object. Will return "nil" if there was no change. 
+crystal_types = "        lapis_lazuli  "
+p crystal_types.strip!
+
+#The partition method is called on the fauna_types variable, which stores the string object "macrofauna"
+#The partition method will return the values given of the result of a search for any value that is given within the argument, in this case the argument is ("a"). It will also return the indexs before and after the argument match. 
+#The puts command will print the string array, ["m", "a", "crofauna"]. 
+fauna_types = "macrofauna"
+p fauna_types.partition("a")
+
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -47,10 +69,38 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+#The div() method is called on the number_of_meals variable, which stores the integer "4".
+#The div() method will the return the outcome of the self value and a selected numeric.
+#The return value is 1 since 4 divided by 4 equals 1. 
+number_of_meals = 4
+p number_of_meals.div(4)
 
+#The <=> is part of the public instance methods. It is being called on the third_place variable which stores the integer 3.
+#The <=> is a comparison. If the self value is less than the numeric a -1 will return. If equal, a 0 will return. If greater, a 1 will return. Nil will return if the self value and numeric are incomparable.
+#The return value will be 0 since the self value and numeric are equal to each other.
+third_place = 3
+p third_place <=> 3
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+#The pred method is being called on an array of negative integers. 
+#The pred method will return the predecessor on each of the integers within the array. The each method is also used since we are iterating the pred method on each of the integers.
+#The return value will give the predecessor to each of the integers sepearatly in the order given. -2,-3,-4,-5. 
+negative_numbers = [-1, -2, -3, -4]
+negative_numbers.each do |num|
+  p num.pred
+end
+#The array method being used here is the shuffle method. A couple other integer methods are being used as well. The split method and sort method. 
+#The split method takes the string of integers and returns the integers as an array. While the shuffle method mixes the integers randomly. Finally, the sort method takes the array of integers and places them into order from least to greater.
+#The return value will be an array with the given integers from least to great. 
+game_numbers = "185469372"
+array_of_game_numbers = game_numbers.split('').shuffle
+array_of_game_numbers.sort!
+p array_of_game_numbers
+
+
+    
--- final_practice.rb
+++ final_practice.rb
@@ -5,12 +5,26 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
+def greeting
+  "Hello today"
+end
+greeting_person_1 = greeting
+p greeting_person_1
+greeting_person_2 = greeting
+p greeting_person_2
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
+def specific_greeting(name)
+  "Hello Today, #{name}"
+end
+greeting_me = specific_greeting("Gabe")
+p greeting_me
+greeting_cat = specific_greeting("Nala")
+p greeting_cat
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -18,6 +32,14 @@
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+def greet_person(name_1, name_2, name_3)
+  "Whats up, #{name_1} #{name_2} #{name_3}"
+end
+greeting_friend_1 = greet_person("Maeg", "Alicia", "Bailey")
+p greeting_friend_1
+greeting_friend_2 = greet_person("Josh", "Freddy", "Bailey")
+p greeting_friend_2
+
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -26,6 +48,15 @@
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+def square(integer)
+  integer * integer
+end
+square_1 = square(7)
+p square_1
+square_2 = square(45)
+p square_2
+
+
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -35,14 +66,25 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+def check_stock(quantity, item)
+  if quantity >= 4
+    "#{item} is stocked"
+  elsif quantity > 0
+    "#{item} - running LOW"
+  else 
+    "#{item} - OUT of stock!"
+  end
+end 
+
+coffee_stock = check_stock(4, "Coffee")  
+p coffee_stock
 # => "Coffee is stocked"
-
-check_stock(3, "Tortillas");
+tortillas_stock = check_stock(3, "Tortillas")
+p tortillas_stock
 # => "Tortillas - running LOW"
-
-check_stock(0, "Cheese");
+cheese_stock = check_stock(0, "Cheese")
+p cheese_stock
 # => "Cheese - OUT of stock!"
-
-check_stock(1, "Salsa");
+salsa_stock = check_stock(1, "Salsa")
+p salsa_stock
 # => "Salsa - running LOW"
